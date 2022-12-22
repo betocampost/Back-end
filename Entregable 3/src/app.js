2322ea9ddd3980635bd2f7d09fff7974f7ac0c59
betocampos <bcamposterrones@gmail.com>
2022-12-22 17:36:05 -0600

    tarea3

---
+++ Entregable 3/src/app.js
@@ -0,0 +1,51 @@
+import express from "express";
+import path from "path";
+import ProductManager from "./ProductManager.js";
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+const productManager = new ProductManager(
+  path.resolve(process.cwd(), "public", "products.json")
+);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/productos", async (req, res) => {
+  try {
+    const products = await productManager.getProducts();
+    const limit = req.query.limit;
+    let limitedProducts;
+    if (limit) {
+      limitedProducts = products.slice(0, limit);
+    }
+    res.send(limitedProducts || products);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+//crear la ruta de la app
+app.post("/productos", async (req, res) => {
+  try {
+    const products = await productManager.getProducts();
+    const newProduct = req.body;
+    await productManager.addProduct(products, newProduct);
+    res.send(newProduct);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+//inicializar el servidor
+
+  app.listen(port, () => {
+  console.log(`Iniciado en http://localhost:${port}`);
+});
